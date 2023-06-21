b792b15bf619ae808053c4ab0064ba9c61c046ed
elis regina <er.elisscherer@gmail.com>
2023-06-21 10:19:05 -0300

    pipe extract

---
+++ extrai_infos_clima.py
@@ -0,0 +1,30 @@
+import os
+from os.path import join
+import pandas as pd
+from datetime import datetime, timedelta
+
+#intervalo de datas
+data_inicio = datetime.today()
+data_fim = data_inicio+ timedelta(days=7)
+
+#formatando as datas
+data_inicio = data_inicio.strftime('%Y-%m-%d')
+data_fim = data_fim.strftime('%Y-%m-%d')
+
+city = 'Boston'
+key = ''
+
+URL = join('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/',
+            f'{city}/{data_inicio}/{data_fim}?unitGroup=metric&include=days&key={key}&contentType=csv')
+#leitura CSV de dados
+dados = pd.read_csv(URL)
+print(dados.head())
+
+# criar caminho pra salvar arquivo
+file_path = f'/home/lia/PycharmProjects/pythonProject/semana={data_inicio}/'
+os.mkdir(file_path)
+
+dados.to_csv(file_path + 'dados_brutos.csv')
+dados[['datetime', 'tempmin', 'temp', 'tempmax']].to_csv(file_path + 'temperaturas.csv')
+dados[['datetime', 'description', 'icon']].to_csv(file_path + 'condicoes.csv')
+
